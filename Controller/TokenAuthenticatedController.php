679ee0b16bee6eb016e8538c23874841549f6fcc
Chris Velarde <chris.velarde@chromedia.com>
2014-04-29 18:49:08 +0800

    Initial structure

---
+++ Controller/TokenAuthenticatedController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * @author Harold Modesto <harold.modesto@chromedia.com>
+ * @author Allejo Chris G. Velarde
+ */
+
+namespace Chromedia\SecurityTokenBundle\Controller;
+
+/**
+ * 
+ *
+ */
+interface TokenAuthenticatedController
+{
+    
+}
---
+++ SecurityTokenBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Chromedia\SecurityTokenBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SecurityTokenBundle extends Bundle
+{
+}
